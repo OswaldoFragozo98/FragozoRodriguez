ed6c6397a6729757245532e71ace506edbce403e
OswaldoFragozo98 <OswaldoFragozo98>
2020-05-07 20:31:49 -0500

    actualización

---
+++ 070520/Clase3/suma/suma.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Suma</title>
+</head>
+<body>
+    <table style="border: 2px solid black" >
+        <form action="">
+            Ingrese el valor del primer numero:
+            <input type="text" name="num1" ><br>
+            Ingrese el valor del segundo numero:
+            <input type="text" name="num2" ><br>
+            Ingrese el valor del tercer numero:
+            <input type="text" name="num3" ><br>
+            <button align="center">Sumar</button>
+        </form>
+</table>
+</body>
+</html>
